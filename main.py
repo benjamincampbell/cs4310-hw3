a52d0d2d65b214e886936f344eab185ffcce097c
Benjamin Mark Campbell <bgf0186@WADE.WMICH.EDU>
2017-10-27 17:31:36 -0400

    report

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from compress import compress
 from decompress import decompress
 import sys
+import time
 
-key, code = compress(sys.argv[1])
+def main():
+    key, code = compress(sys.argv[1])
     
-decompress(code, key)
+    decompress(code, key)
+    
+    
+if __name__ == '__main__':
+    results = []
+    for i in range(0, 1000):
+        start = time.time()
+        main()
+        finish = time.time()
+        results.append(finish - start)
+        
+    print("Avg run time for 1000 runs: {} milliseconds".format((sum(results) / len(results)) * 1000))
